b0cf948fb7f39939b37bc0a0b7af8c33af6247fc
wulinyi <wulinyi22@gmail.com>
2025-08-18 17:40:05 +0800

    🎉 init(初始化,vue3JS+Tailwind+Antd Vue): pinia状态管理,vue-router

---
+++ Ticket-Management-and-Chart-Display-Page/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+    <Button type="primary">Primary Button</Button>
+  </h1>
+</template>
+
+<script setup>
+import { Button } from 'ant-design-vue';
+</script>
